1154fb8e07304da4a0fb77b05a62c679c1836031
Ivan Kaliuzhnyi <i.kaliuzhnyi@zakaz.global>
2024-09-22 19:20:34 +0300

    modify side-by-side smart view

--- src/main/java/com/vaadin/flow/component/smart/view/side/SideBySideSmartView.java
+++ src/main/java/com/vaadin/flow/component/smart/view/side/SideBySideSmartView.java
@@ -15,4 +15,23 @@ public interface SideBySideSmartView
     @Nonnull
     <T extends Component & FlexComponent & HasSize> T getSecondarySide();
 
+    @Override
+    default void adjustBodyLayoutForScreen() {
+        BaseSmartView.super.adjustBodyLayoutForScreen();
+        adjustPrimarySideForScreen();
+        adjustSecondarySideForScreen();
+    }
+
+    default void adjustPrimarySideForScreen() {
+        // Do nothing here
+        // It's just stub method
+        // This method is only for overriding
+    }
+
+    default void adjustSecondarySideForScreen() {
+        // Do nothing here
+        // It's just stub method
+        // This method is only for overriding
+    }
+
 }
