ad5a2e61bfa5dd40ec553cd17e7a80b194156f25
Ivan Kaliuzhnyi <i.kaliuzhnyi@zakaz.global>
2024-09-24 17:31:22 +0300

    modify application

--- src/main/java/com/vaadin/flow/smart/Application.java
+++ src/main/java/com/vaadin/flow/smart/Application.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-@Theme(value = "vaadin-smart-view")
+@Theme(value = "vaadin-smart-components")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
