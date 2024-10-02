d0e045ca7dc909ee2eb5d9078754bcde099035d2
Ivan Kaliuzhnyi <i.kaliuzhnyi@zakaz.global>
2024-10-02 16:31:39 +0300

    restructure

--- src/main/java/com/vaadin/flow/smart/examples/ExampleInfoBlockSmartComponentView.java
+++
@@ -1,8 +0,0 @@
-package com.vaadin.flow.smart.examples;
-
-import com.vaadin.flow.component.orderedlayout.FlexLayout;
-import com.vaadin.flow.smart.view.side.block.textblock.AbstractTextBlockByImageSmartView;
-
-public class ExampleInfoBlockSmartComponentView
-        extends AbstractTextBlockByImageSmartView<FlexLayout, ExampleTextBlockSmartComponent> {
-}
