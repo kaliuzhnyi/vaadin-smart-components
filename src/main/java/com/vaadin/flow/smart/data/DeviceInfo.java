d64f7c714bb9d4ae1797b6309faeaf8b9115b7f9
Ivan Kaliuzhnyi <i.kaliuzhnyi@zakaz.global>
2024-10-14 15:04:30 +0300

    fix

--- src/main/java/com/vaadin/flow/smart/data/DeviceInfo.java
+++ src/main/java/com/vaadin/flow/smart/data/DeviceInfo.java
@@ -13,4 +13,36 @@ public class DeviceInfo {
 
     private String userAgent;
 
+    public boolean isMobile() {
+        return deviceType.equals(DeviceType.Mobile);
+    }
+
+    public boolean isTablet() {
+        return deviceType.equals(DeviceType.Tablet);
+    }
+
+    public boolean isDesktop() {
+        return deviceType.equals(DeviceType.Desktop);
+    }
+
+    public boolean isWindows() {
+        return devicePlatform.equals(DevicePlatform.Windows);
+    }
+
+    public boolean isLinux() {
+        return devicePlatform.equals(DevicePlatform.Linux);
+    }
+
+    public boolean isMacOS() {
+        return devicePlatform.equals(DevicePlatform.MacOS);
+    }
+
+    public boolean isIOS() {
+        return devicePlatform.equals(DevicePlatform.iOS);
+    }
+
+    public boolean isAndroid() {
+        return devicePlatform.equals(DevicePlatform.Android);
+    }
+
 }
