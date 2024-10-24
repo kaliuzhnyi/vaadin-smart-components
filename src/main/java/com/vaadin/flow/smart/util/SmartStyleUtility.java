caac69d117561d7a993a4a221bd4435e11b2539a
Ivan Kaliuzhnyi <i.kaliuzhnyi@zakaz.global>
2024-10-24 12:38:53 +0300

    add styles

---
+++ src/main/java/com/vaadin/flow/smart/util/SmartStyleUtility.java
@@ -0,0 +1,14 @@
+package com.vaadin.flow.smart.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class SmartStyleUtility {
+
+    public static final class Overflow {
+
+        public static final String HIDE = "scrollbar-hide";
+
+    }
+
+}
